4d5610b9835b2ebfccca8300d6428d785834189f
Morgese Luca (ACC0112983) <luca.morgese@sci-b106pc006x.unitn.it>
2018-12-05 11:33:15 +0100

    implemented first phase

---
+++ app.js
@@ -0,0 +1,27 @@
+
+const express = require('express');
+const port = process.env.PORT || 3000;
+const app = express();
+
+function string_square(s) {
+    if (typeof s === 'string') {
+        let length = s.length;
+        return length * length;
+    } else {
+        return -1;
+    }
+}
+
+app.get('/', function (req, res) {
+    res.send('Hello there!');
+});
+
+app.get('/square', function(req, res) {
+    let stringValue = req.query.string;
+    let value = string_square(stringValue);
+    res.json({"result": value});
+});
+
+app.listen(port, () => {
+    console.log('App listening on port: ' + port);
+});
